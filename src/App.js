ed0dbc48dfbe5566479fff98241fc74b0794e09f
bvp1518 <bvp18061992@gmail.com>
2024-08-09 13:21:16 +0530

    till-215. State Scheduling & Batching

--- src/App.js
+++ src/App.js
@@ -13,6 +13,8 @@ function App() {
 
   function handleSetCount(newCount){
     setChosenCount(newCount);
+    setChosenCount((prevChosenCount) => prevChosenCount + 1);
+    // console.log(chosenCount);
   }
 
   return (
@@ -20,7 +22,7 @@ function App() {
       <Header />
       <main>
         <ConfigureCounter onSet={handleSetCount} />
-        <Counter initialCount={chosenCount} />
+        <Counter key={chosenCount} initialCount={chosenCount} />
         <Counter initialCount={0} />
       </main>
     </>
